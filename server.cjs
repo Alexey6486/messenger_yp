fe21b9b710683581de9675f88aed1d1e2985f32b
alexey6486 <alexey6486@gmail.com>
2025-11-19 08:54:52 +0300

    deploy commit

--- server.cjs
+++ server.cjs
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 app.use(express.static('./dist'));
 
-app.get('/*route', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './dist/index.html'));
 
     res.status(200);
