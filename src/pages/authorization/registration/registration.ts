e47cbc1a85b557b0c5c71069607e49cfbe0bc7d3
Alex6486 <achernov@bellintegrator.com>
2025-11-23 23:10:20 +0300

    sprint 1

--- src/pages/authorization/registration/registration.ts
+++ src/pages/authorization/registration/registration.ts
@@ -1,6 +1,6 @@
 import * as Handlebars from 'handlebars';
 import template from './registration-template.hbs?raw';
-import {IDS, PAGES} from '@/constants';
+import { IDS, PAGES } from '@/constants';
 import type { IFormState, IRegistrationFormUi } from '@/types';
 import styles from '../styles.module.pcss';
 
