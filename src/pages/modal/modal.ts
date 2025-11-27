1d95ea2194b9af44e5b875821e667b4a0737544e
Alex6486 <achernov@bellintegrator.com>
2025-11-27 13:14:07 +0300

    sprint 1

--- src/pages/modal/modal.ts
+++ src/pages/modal/modal.ts
@@ -1,12 +1,10 @@
 import * as Handlebars from 'handlebars';
 import template from './modal-template.hbs?raw';
 import { PARTIALS_TYPES } from '@/constants';
-import styles from './styles.module.pcss';
 
 export const GetModal = (state: any, partialName: string) => {
     const content = Handlebars.compile(template);
     return content({
-        styles,
         state,
         partialName,
         partialType: PARTIALS_TYPES.MODAL,
