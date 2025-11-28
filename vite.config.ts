90a7423a35925bb9709f998988e4626786bed1aa
Alex6486 <achernov@bellintegrator.com>
2025-11-28 18:09:59 +0300

    deploy gh

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import * as path from 'path';
 import postcssPresetEnv from 'postcss-preset-env';
 import handlebars from 'vite-plugin-handlebars';
 
@@ -10,7 +11,7 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            '@': '/src',
+            '@': path.resolve(__dirname, './src'),
         },
     },
     css: {
