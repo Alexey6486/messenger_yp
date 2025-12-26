87aa2db010b6a7ebea364fa35e5be94f82ec835a
alexey6486 <alexey6486@gmail.com>
2025-12-26 16:09:06 +0300

    sprint 3

--- src/utils/clone-deep.ts
+++ src/utils/clone-deep.ts
@@ -1,13 +1,20 @@
-import type { TObjectUnknown } from '@/types/general';
 
-export function cloneDeep<T extends object = object>(obj: T) {
-    return (function cloneDeepInner(item: T): T | Date | Set<unknown> | Map<unknown, unknown> | object | T[] {
+type CloneableResult<T> =
+	T extends Date ? Date :
+		T extends Set<unknown> ? Set<unknown> :
+			T extends Map<unknown, unknown> ? Map<unknown, unknown> :
+				T extends Array<infer U> ? Array<CloneableResult<U>> :
+					T extends object ? { [K in keyof T]: CloneableResult<T[K]> } :
+						T;
+
+export function cloneDeep<T = unknown>(obj: T): CloneableResult<T> {
+    return (function cloneDeepInner<U>(item: U): CloneableResult<U> {
         if (item === null || typeof item !== "object") {
-            return item;
+            return item as CloneableResult<U>;
         }
 
         if (item instanceof Date) {
-            return new Date(item.valueOf());
+            return new Date(item.valueOf()) as CloneableResult<U>;
         }
 
         if (item instanceof Array) {
@@ -17,7 +24,7 @@ export function cloneDeep<T extends object = object>(obj: T) {
 				copy[i] = cloneDeepInner(item[i]) as T;
 			}
 
-            return copy;
+            return copy as CloneableResult<U>;
         }
 
         if (item instanceof Set) {
@@ -25,7 +32,7 @@ export function cloneDeep<T extends object = object>(obj: T) {
 
             item.forEach(v => copy.add(cloneDeepInner(v)));
 
-            return copy;
+            return copy as CloneableResult<U>;
         }
 
         if (item instanceof Map) {
@@ -33,14 +40,25 @@ export function cloneDeep<T extends object = object>(obj: T) {
 
             item.forEach((v, k) => copy.set(k, cloneDeepInner(v)));
 
-            return copy;
+            return copy as CloneableResult<U>;
         }
 
         if (item instanceof Object) {
-			const copy: TObjectUnknown = {};
-			Object.keys(item as TObjectUnknown).forEach(k => (copy[k] = cloneDeepInner((item as TObjectUnknown)[k] as T)));
-            Object.getOwnPropertySymbols(item as TObjectUnknown).forEach(s => (copy[s] = cloneDeepInner((item as TObjectUnknown)[s] as T)));
-            return copy;
+			// const copy: TObjectUnknown = {};
+			// Object.keys(item as TObjectUnknown).forEach(k => (copy[k] = cloneDeepInner((item as TObjectUnknown)[k] as T)));
+            // Object.getOwnPropertySymbols(item as TObjectUnknown).forEach(s => (copy[s] = cloneDeepInner((item as TObjectUnknown)[s] as T)));
+
+			let copy: Record<string, unknown> = {};
+            // Object.keys(item).forEach(k => (copy[k] = cloneDeepInner(item[k])));
+            // return copy as CloneableResult<U>;
+
+			const keys = Object.keys(item) as Array<keyof typeof item>;
+			keys.forEach(k => {
+				const key = k as string;
+				(copy as Record<string, unknown>)[key] = cloneDeepInner(
+					(item as Record<string, unknown>)[key]
+				);
+			});
         }
 
         throw new Error(`Unable to copy object: ${item}`);
