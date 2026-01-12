74883d06a74320922ebf68653f85368f5e69d401
Alex6486 <achernov@bellintegrator.com>
2026-01-12 20:07:43 +0300

    deploy gh

--- src/router/router.ts
+++ src/router/router.ts
@@ -97,7 +97,7 @@ export class Router {
 	start() {
 		window.onpopstate = ((event: PopStateEvent) => {
 			if (event.currentTarget instanceof Window) {
-				this._onRoute(event.currentTarget.location.hash);
+				this._onRoute(event.currentTarget.location.pathname);
 			}
 		}).bind(this);
 
@@ -105,7 +105,7 @@ export class Router {
 			AuthController.logout();
 		}).bind(this);
 
-		this._onRoute(window.location.hash);
+		this._onRoute(window.location.pathname);
 	}
 
 	_onRoute(pathname: string) {
